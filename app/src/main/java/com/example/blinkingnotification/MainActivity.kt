590e2a7e03eb6c55874141aac757741575135756
minWachya <alal7272@naver.com>
2022-01-24 16:51:33 +0900

    서브 메뉴 달기

--- app/src/main/java/com/example/blinkingnotification/MainActivity.kt
+++ app/src/main/java/com/example/blinkingnotification/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.blinkingnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.blinkingnotification.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // 서브 메뉴 달기
+        binding.toolBar.inflateMenu(R.menu.sub_menu)
 
     }
 }
