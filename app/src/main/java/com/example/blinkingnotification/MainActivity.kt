fcf28b23b1eac14fc61c30ed06ca793931af89cc
minWachya <alal7272@naver.com>
2022-01-24 17:05:49 +0900

    옵션 메뉴 달기

--- app/src/main/java/com/example/blinkingnotification/MainActivity.kt
+++ app/src/main/java/com/example/blinkingnotification/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.blinkingnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.blinkingnotification.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
@@ -14,7 +17,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         // 서브 메뉴 달기
-        binding.toolBar.inflateMenu(R.menu.sub_menu)
+        setSupportActionBar(binding.toolBar)
 
     }
+
+
+    // 서브 메뉴 등록
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.sub_menu, menu)
+        return true
+    }
+
+    // 메뉴 클릭 시 동작하는 메소드
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.help -> {
+                Toast.makeText(applicationContext, "도움!", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.ask -> {
+                return true
+            }
+        }
+        return false
+    }
+
 }
