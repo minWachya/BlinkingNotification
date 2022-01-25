509ee0ab44b72b3157a84c0519d762e60c99b9c0
minWachya <alal7272@naver.com>
2022-01-25 17:19:20 +0900

    알림 형식 스피너 기능 추가

--- app/src/main/java/com/example/blinkingnotification/SetAlarmActivity.kt
+++ app/src/main/java/com/example/blinkingnotification/SetAlarmActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import com.example.blinkingnotification.databinding.ActivitySetAlramBinding
 
@@ -18,7 +19,7 @@ class SetAlarmActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        // 반복 시간 설정 스피너
+        // 반복 시간 스피너 설정
         // 배열 추가
         val selectTimeSpinner = binding.spinnerSelectTime
         ArrayAdapter.createFromResource(
@@ -42,5 +43,43 @@ class SetAlarmActivity : AppCompatActivity() {
             }
             selectTimeSpinner.onItemSelectedListener = selectTimeSpinnerAdapter
         }
+
+        // 알림 형식 스피너 설정
+        // 배열 추가
+        val selectModeSpinner = binding.spinnerSelectMode
+        ArrayAdapter.createFromResource(
+            this@SetAlarmActivity,
+            R.array.select_mode_array,
+            android.R.layout.simple_dropdown_item_1line
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            selectModeSpinner.adapter = adapter
+            // 어댑터 추가
+            val selectModeSpinnerAdapter = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    when(position) {
+                        0 -> {
+                            // 4. 이미지 선택 안보이게
+                            binding.textView4.visibility = View.INVISIBLE
+                            binding.view4.visibility = View.INVISIBLE
+                            binding.btnSelectImage.visibility = View.INVISIBLE
+                        }
+                        1 -> {
+                            // 4. 이미지 선택 보이기
+                            binding.textView4.visibility = View.VISIBLE
+                            binding.view4.visibility = View.VISIBLE
+                            binding.btnSelectImage.visibility = View.VISIBLE
+                            Toast.makeText(applicationContext, "이미지", Toast.LENGTH_SHORT).show()
+                        }
+                        else -> { Toast.makeText(applicationContext, "다시 선택해주세요.", Toast.LENGTH_SHORT).show() }
+                    }
+                }
+            }
+            selectModeSpinner.onItemSelectedListener = selectModeSpinnerAdapter
+        }
+
     }
+
 }
