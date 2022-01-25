6d03e6b23b7742ee650728fea2fdd0224e0626f9
minWachya <alal7272@naver.com>
2022-01-25 16:36:09 +0900

    그냥 스플래시로 합의

--- app/src/main/java/com/example/blinkingnotification/SplashActivity.kt
+++ app/src/main/java/com/example/blinkingnotification/SplashActivity.kt
@@ -4,13 +4,18 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.blinkingnotification.databinding.ActivitySplashBinding
+
+private lateinit var binding: ActivitySplashBinding
 
 // 스플래시
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // 메인 액티비티로 이동
         val intent = Intent(this@SplashActivity, MainActivity::class.java)
