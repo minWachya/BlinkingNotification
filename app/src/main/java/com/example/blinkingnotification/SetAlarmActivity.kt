de4599d3fc4e7ae744a43e6e947522522caafe49
minWachya <alal7272@naver.com>
2022-01-25 16:59:26 +0900

    반복 시간 설정 스피너 기능 추가

--- app/src/main/java/com/example/blinkingnotification/SetAlarmActivity.kt
+++ app/src/main/java/com/example/blinkingnotification/SetAlarmActivity.kt
@@ -2,6 +2,10 @@ package com.example.blinkingnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.blinkingnotification.databinding.ActivitySetAlramBinding
 
 private lateinit var binding: ActivitySetAlramBinding
@@ -13,5 +17,30 @@ class SetAlarmActivity : AppCompatActivity() {
         binding = ActivitySetAlramBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // 반복 시간 설정 스피너
+        // 배열 추가
+        val selectTimeSpinner = binding.spinnerSelectTime
+        ArrayAdapter.createFromResource(
+            this@SetAlarmActivity,
+            R.array.select_time_array,
+            android.R.layout.simple_dropdown_item_1line
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            selectTimeSpinner.adapter = adapter
+            // 어댑터 추가
+            val selectTimeSpinnerAdapter = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    when(position) {
+                        0 -> { Toast.makeText(applicationContext, "1분", Toast.LENGTH_SHORT).show() }
+                        1 -> { Toast.makeText(applicationContext, "5분", Toast.LENGTH_SHORT).show() }
+                        else -> { Toast.makeText(applicationContext, "다른 거", Toast.LENGTH_SHORT).show() }
+                    }
+                }
+            }
+            selectTimeSpinner.onItemSelectedListener = selectTimeSpinnerAdapter
+        }
     }
 }
