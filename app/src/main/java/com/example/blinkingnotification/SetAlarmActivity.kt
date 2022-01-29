d5160eb43d67fad380512534354650a508b71d8a
minWachya <alal7272@naver.com>
2022-01-29 19:36:41 +0900

    갤러이에서 사진 가져오기 기능 추가

--- app/src/main/java/com/example/blinkingnotification/SetAlarmActivity.kt
+++ app/src/main/java/com/example/blinkingnotification/SetAlarmActivity.kt
@@ -1,5 +1,6 @@
 package com.example.blinkingnotification
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,7 +10,13 @@ import android.widget.Spinner
 import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import com.example.blinkingnotification.databinding.ActivitySetAlramBinding
+import android.content.Intent
+import android.graphics.BitmapFactory
+import androidx.activity.result.contract.ActivityResultContracts
 
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+private const val PICK_FROM_ALBUM = 0
 private lateinit var binding: ActivitySetAlramBinding
 
 // 알림 설정 화면
@@ -19,6 +26,16 @@ class SetAlarmActivity : AppCompatActivity() {
         binding = ActivitySetAlramBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // 갤러리에서 사진 선택 후 실행
+        val getFromAlbumResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val uri = result.data?.data // 선택한 이미지의 주소
+                // 이미지 파일 읽어와서 설정하기
+                if (uri != null) binding.imgView.setImageBitmap(BitmapFactory.decodeStream(contentResolver.openInputStream(uri)))
+                else binding.imgView.setImageResource(R.drawable.ic_launcher_background)
+            }
+        }
 
         // 제목/내용 입력할 때마다 실시간으로 미리보기 변경
         binding.editTitle.doOnTextChanged { _, _, _, _ ->
@@ -30,7 +47,7 @@ class SetAlarmActivity : AppCompatActivity() {
             binding.tvContent.text = content
         }
 
-        // 반복 시간 스피너 설정
+        // 2번: 반복 시간 스피너 설정
         // 배열 추가
         val selectTimeSpinner = binding.spinnerSelectTime
         ArrayAdapter.createFromResource(
@@ -55,7 +72,7 @@ class SetAlarmActivity : AppCompatActivity() {
             selectTimeSpinner.onItemSelectedListener = selectTimeSpinnerAdapter
         }
 
-        // 알림 형식 스피너 설정
+        // 3번: 알림 형식 스피너 설정
         // 배열 추가
         val selectModeSpinner = binding.spinnerSelectMode
         ArrayAdapter.createFromResource(
@@ -76,19 +93,29 @@ class SetAlarmActivity : AppCompatActivity() {
                             binding.textView4.visibility = View.INVISIBLE
                             binding.view4.visibility = View.INVISIBLE
                             binding.btnSelectImage.visibility = View.INVISIBLE
+                            binding.imgView.visibility = View.INVISIBLE
                         }
                         1 -> {
                             // 4. 이미지 선택 보이기
                             binding.textView4.visibility = View.VISIBLE
                             binding.view4.visibility = View.VISIBLE
                             binding.btnSelectImage.visibility = View.VISIBLE
-                            Toast.makeText(applicationContext, "이미지", Toast.LENGTH_SHORT).show()
+                            binding.imgView.visibility = View.VISIBLE
                         }
                         else -> { Toast.makeText(applicationContext, "다시 선택해주세요.", Toast.LENGTH_SHORT).show() }
                     }
                 }
             }
             selectModeSpinner.onItemSelectedListener = selectModeSpinnerAdapter
+        }
+
+        // 4번: 이미지 설정
+        // 이미지 추가하기
+        binding.btnSelectImage.setOnClickListener {
+            // 갤러리에서 사진 선택해서 가져오기
+            val intent = Intent("android.intent.action.GET_CONTENT")
+            intent.type = "image/*"     // 모든 이미지
+            getFromAlbumResultLauncher.launch(intent)
         }
 
     }
