6ba1c8ac7c3b2cd1d8d0cac5830782d701370439
minWachya <alal7272@naver.com>
2022-01-30 16:38:24 +0900

    사진 회전되는 문제 고치기

--- app/src/main/java/com/example/blinkingnotification/SetAlarmActivity.kt
+++ app/src/main/java/com/example/blinkingnotification/SetAlarmActivity.kt
@@ -6,21 +6,31 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import com.example.blinkingnotification.databinding.ActivitySetAlramBinding
 import android.content.Intent
 import android.graphics.BitmapFactory
 import androidx.activity.result.contract.ActivityResultContracts
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import androidx.annotation.RequiresApi
+import java.io.IOException
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import java.lang.Exception
 
-import androidx.core.app.ActivityCompat.startActivityForResult
 
+private const val TAG = "mmm"
 private const val PICK_FROM_ALBUM = 0
 private lateinit var binding: ActivitySetAlramBinding
 
 // 알림 설정 화면
 class SetAlarmActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySetAlramBinding.inflate(layoutInflater)
@@ -32,7 +42,17 @@ class SetAlarmActivity : AppCompatActivity() {
             if (result.resultCode == Activity.RESULT_OK) {
                 val uri = result.data?.data // 선택한 이미지의 주소
                 // 이미지 파일 읽어와서 설정하기
-                if (uri != null) binding.imgView.setImageBitmap(BitmapFactory.decodeStream(contentResolver.openInputStream(uri)))
+                if (uri != null) {
+                    // 사진 가져오기
+                    val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(uri))
+                    // 사진의 회전 정보 가져오기
+                    val orientation = getOrientationOfImage(uri).toFloat()
+                    // 이미지 회전하기
+                    val newBitmap = getRotatedBitmap(bitmap, orientation)
+                    // 회전된 이미지로 imaView 설정
+                    binding.imgView.setImageBitmap(newBitmap)
+
+                }
                 else binding.imgView.setImageResource(R.drawable.ic_launcher_background)
             }
         }
@@ -119,5 +139,40 @@ class SetAlarmActivity : AppCompatActivity() {
         }
 
     }
+
+    // 이미지 회전 정보 가져오기
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getOrientationOfImage(uri: Uri): Int {
+        // uri -> 절대경로
+        val inputStream = contentResolver.openInputStream(uri)
+        val exif: ExifInterface? = try {
+            ExifInterface(inputStream!!)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return -1
+        }
+        inputStream.close()
+
+        val orientation = exif?.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+        if (orientation != -1) {
+            when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> return 90
+                ExifInterface.ORIENTATION_ROTATE_180 -> return 180
+                ExifInterface.ORIENTATION_ROTATE_270 -> return 270
+            }
+        }
+        return 0
+    }
+
+    // 이미지 회전하기
+    @Throws(Exception::class)
+    private fun getRotatedBitmap(bitmap: Bitmap?, degrees: Float): Bitmap? {
+        if (bitmap == null) return null
+        if (degrees == 0F) return bitmap
+        val m = Matrix()
+        m.setRotate(degrees, bitmap.width.toFloat() / 2, bitmap.height.toFloat() / 2)
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, m, true)
+    }
+
 
 }
